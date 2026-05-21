168da354ffff8e682cbe214b7fb9319fd5cb6583
nuomin2 <152868765+nuomin2@users.noreply.github.com>
2026-05-21 22:33:19 +0800

    VS Code Light+语法高亮替换、文件资源管理器图标添加、删除占位面板、树节点间距优化

--- Pytools/Views/MainWindow.xaml.cs
+++ Pytools/Views/MainWindow.xaml.cs
@@ -4,8 +4,12 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Threading;
+using System.Xml;
 using AvalonDock.Layout;
 using ICSharpCode.AvalonEdit;
+using ICSharpCode.AvalonEdit.Highlighting;
+using ICSharpCode.AvalonEdit.Highlighting.Xshd;
+using MaterialDesignThemes.Wpf;
 using Pytools.Models;
 using Pytools.ViewModels;
 
@@ -67,15 +71,77 @@ namespace Pytools.Views
                     RemoveTab(doc);
         }
 
+        private static IHighlightingDefinition? _vsCodeLightHighlighting;
+
+        private static IHighlightingDefinition GetVSCodeLightHighlighting()
+        {
+            if (_vsCodeLightHighlighting != null) return _vsCodeLightHighlighting;
+
+            string xshd = @"<?xml version=""1.0""?>
+    <SyntaxDefinition name=""PythonVSCodeLight"" extensions="".py"" xmlns=""http://icsharpcode.net/sharpdevelop/syntaxdefinition/2008"">
+        <Color name=""Comment"" foreground=""#008000"" />
+        <Color name=""String"" foreground=""#A31515"" />
+        <Color name=""Keyword"" foreground=""#0000FF"" />
+        <Color name=""ControlKeyword"" foreground=""#AF00DB"" />
+        <Color name=""BuiltIn"" foreground=""#2B91AF"" />
+        <Color name=""Number"" foreground=""#098658"" />
+        <RuleSet ignoreCase=""false"">
+            <Span color=""Comment"" begin=""#"" />
+            <Span color=""String"" multiline=""true"">
+                <Begin>'''</Begin><End>'''</End>
+            </Span>
+            <Span color=""String"" multiline=""true"">
+                <Begin>&quot;&quot;&quot;</Begin><End>&quot;&quot;&quot;</End>
+            </Span>
+            <Span color=""String"">
+                <Begin>&quot;</Begin><End>&quot;</End>
+                <RuleSet><Span begin=""\\"" end=""."" /></RuleSet>
+            </Span>
+            <Span color=""String"">
+                <Begin>'</Begin><End>'</End>
+                <RuleSet><Span begin=""\\"" end=""."" /></RuleSet>
+            </Span>
+            <Keywords color=""Keyword"">
+                <Word>class</Word><Word>def</Word><Word>global</Word><Word>nonlocal</Word>
+                <Word>lambda</Word><Word>assert</Word><Word>del</Word><Word>pass</Word>
+            </Keywords>
+            <Keywords color=""ControlKeyword"">
+                <Word>and</Word><Word>as</Word><Word>break</Word><Word>continue</Word>
+                <Word>elif</Word><Word>else</Word><Word>except</Word><Word>False</Word>
+                <Word>finally</Word><Word>for</Word><Word>from</Word><Word>if</Word>
+                <Word>import</Word><Word>in</Word><Word>is</Word><Word>not</Word>
+                <Word>or</Word><Word>raise</Word><Word>return</Word><Word>True</Word>
+                <Word>try</Word><Word>while</Word><Word>with</Word><Word>yield</Word><Word>None</Word>
+            </Keywords>
+            <Keywords color=""BuiltIn"">
+                <Word>print</Word><Word>len</Word><Word>range</Word><Word>int</Word>
+                <Word>float</Word><Word>str</Word><Word>list</Word><Word>dict</Word>
+                <Word>set</Word><Word>tuple</Word><Word>type</Word><Word>super</Word>
+            </Keywords>
+            <Rule color=""Number"">
+                \b0[xX][0-9a-fA-F]+|(\b\d+(\.[0-9]+)?|\.[0-9]+)([eE][+-]?[0-9]+)?
+            </Rule>
+        </RuleSet>
+    </SyntaxDefinition>";
+
+            using (var reader = new System.Xml.XmlTextReader(new System.IO.StringReader(xshd)))
+            {
+                _vsCodeLightHighlighting = ICSharpCode.AvalonEdit.Highlighting.Xshd.HighlightingLoader.Load(reader, ICSharpCode.AvalonEdit.Highlighting.HighlightingManager.Instance);
+            }
+            return _vsCodeLightHighlighting;
+        }
+
         private void AddTab(DocumentModel doc)
         {
             var editor = new TextEditor
             {
-                FontFamily = new FontFamily("Consolas"),
+                FontFamily = new System.Windows.Media.FontFamily("Consolas"),
                 FontSize = 13,
                 ShowLineNumbers = true,
-                SyntaxHighlighting = ICSharpCode.AvalonEdit.Highlighting.HighlightingManager.Instance.GetDefinition("Python"),
-                VerticalScrollBarVisibility = ScrollBarVisibility.Auto,
+                SyntaxHighlighting = GetVSCodeLightHighlighting(),
+                Background = System.Windows.Media.Brushes.White,
+                Foreground = System.Windows.Media.Brushes.Black,
+                VerticalScrollBarVisibility = System.Windows.Controls.ScrollBarVisibility.Auto,
                 Text = doc.Content
             };
 
@@ -170,30 +236,63 @@ namespace Pytools.Views
 
         private TreeViewItem CreateDirectoryNode(string path)
         {
-            var node = new TreeViewItem
-            {
-                Header = System.IO.Path.GetFileName(path),
-                Tag = path
-            };
+            var node = new TreeViewItem { Tag = path };
 
-            if (string.IsNullOrEmpty(node.Header.ToString()))
-                node.Header = path;
+            var folderStack = new StackPanel { Orientation = Orientation.Horizontal };
+            folderStack.Children.Add(new PackIcon
+            {
+                Kind = PackIconKind.FolderOutline,
+                Width = 16, Height = 16,
+                Margin = new System.Windows.Thickness(0, 0, 6, 0),
+                Foreground = Brushes.Goldenrod,
+                VerticalAlignment = System.Windows.VerticalAlignment.Center
+            });
+            folderStack.Children.Add(new TextBlock
+            {
+                Text = System.IO.Path.GetFileName(path),
+                VerticalAlignment = System.Windows.VerticalAlignment.Center
+            });
+            node.Header = folderStack;
+
+            if (string.IsNullOrEmpty(System.IO.Path.GetFileName(path)))
+            {
+                ((TextBlock)folderStack.Children[1]).Text = path;
+            }
 
             try
             {
-                foreach (string directory in Directory.GetDirectories(path))
+                foreach (string directory in System.IO.Directory.GetDirectories(path))
                     node.Items.Add(CreateDirectoryNode(directory));
 
-                foreach (string file in Directory.GetFiles(path))
+                foreach (string file in System.IO.Directory.GetFiles(path))
                 {
-                    node.Items.Add(new TreeViewItem
+                    var fileNode = new TreeViewItem { Tag = file };
+
+                    var fileStack = new StackPanel { Orientation = Orientation.Horizontal };
+
+                    bool isPython = file.EndsWith(".py", System.StringComparison.OrdinalIgnoreCase);
+                    PackIconKind iconKind = isPython ? PackIconKind.LanguagePython : PackIconKind.FileDocumentOutline;
+                    Brush iconColor = isPython ? Brushes.CornflowerBlue : Brushes.Gray;
+
+                    fileStack.Children.Add(new PackIcon
                     {
-                        Header = System.IO.Path.GetFileName(file),
-                        Tag = file
+                        Kind = iconKind,
+                        Width = 16, Height = 16,
+                        Margin = new System.Windows.Thickness(0, 0, 6, 0),
+                        Foreground = iconColor,
+                        VerticalAlignment = System.Windows.VerticalAlignment.Center
                     });
+                    fileStack.Children.Add(new TextBlock
+                    {
+                        Text = System.IO.Path.GetFileName(file),
+                        VerticalAlignment = System.Windows.VerticalAlignment.Center
+                    });
+
+                    fileNode.Header = fileStack;
+                    node.Items.Add(fileNode);
                 }
             }
-            catch (UnauthorizedAccessException) { }
+            catch (System.UnauthorizedAccessException) { }
 
             return node;
         }
